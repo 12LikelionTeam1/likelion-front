38edd4630e7cf763e1bc3c161faed3354f389aa6
K4NEI <kimyoungho0415@gmail.com>
2024-07-10 16:33:37 +0900

    feat/black: datepicker updated

--- daily-tales-app/src/assets/images.ts
+++ daily-tales-app/src/assets/images.ts
@@ -1,7 +1,15 @@
 const IMAGE_ROOT = '/images';
 
 const images = {
+  icons: {
+    calendar: IMAGE_ROOT + '/icons/ic-calendar.png',
+    user: IMAGE_ROOT + '/icons/ic-user.png',
+    close: IMAGE_ROOT + '/icons/ic-close.png',
+    prev: IMAGE_ROOT + '/icons/ic-prev.png',
+    next: IMAGE_ROOT + '/icons/ic-next.png',
+  },
   logo96: IMAGE_ROOT + '/logo-96.png',
+  logos: IMAGE_ROOT + '/logo-s.png',
   googleLogo: IMAGE_ROOT + '/ic-google-logo.png',
 };
 
